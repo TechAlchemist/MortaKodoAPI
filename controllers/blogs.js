2602cca338765246aba139f8cf8ab36725a91bbb
Brandon Myers <brandonRmyers1512@gmail.com>
2021-06-14 13:43:01 -0700

    Adding update and delete to blog controller.

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -13,30 +13,63 @@ async function createNewBlog(req, res) {
 
 async function getAllBlogs(req, res) {
     let sliceArray = false;
+    const results = {};
     if (req.query.page && req.query.limit) {
-        const page = req.query.page;
-        const limit = req.query.limit;
-        // scoped as var to reach slice in find
+        var page = parseInt(req.query.page);
+        var limit = parseInt(req.query.limit);
         var startIndex = (page - 1) * limit;
         var endIndex = page * limit;
         sliceArray = true;
     }
     Blog.find({}, (error, articles) => {
+        results.results = articles;
         if (error) return res.send(500).json('Error fetching blogs.');
-        if (sliceArray) articles = articles.slice(startIndex, endIndex)
-        return res.status(200).json(articles);
+        // format data for pagination
+        if (sliceArray) {
+            results.results = articles.slice(startIndex, endIndex);
+            if (endIndex <  articles.length) {
+                results.next = {
+                    page: page + 1,
+                    limit: limit
+                }
+            }
+            if (startIndex > 0) {
+                results.previous = {
+                    page: page - 1,
+                    limit: limit
+                }
+            }
+        }
+        return res.status(200).json(results);
     });
 }
 
 async function getBlog(req, res) {
-    Blog.find({ _id: req.params._id}, (error, article) => {
+    Blog.find({ _id: req.params.id}, (error, article) => {
         if (error) return res.status(500).json('Failed to retrive article.');
         return res.status(200).json(article);
     })
 }
 
+async function deleteBlog(req, res) {
+    Blog.deleteOne({ _id: req.params.id}, (error) => {
+        if (error) return res.status(500).json(error);
+        return res.status(200).json('Blog article deleted.');
+    });
+}
+
+async function updateBlog(req, res) {
+    Blog.findOneAndUpdate(req.params.id, req.body, {useFindAndModify: false}, (error, article) => {
+        if (error) res.status(500).json('error');
+        return res.status(200).json('updated')
+   });
+    
+}
+
 module.exports = {
     createNewBlog,
     getAllBlogs,
-    getBlog
+    getBlog,
+    deleteBlog,
+    updateBlog
 }
--- routes/api/v1/blogs.js
+++ routes/api/v1/blogs.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const blogController = require('../../../controllers/blogs');
 
 router.post('/createBlog', blogController.createNewBlog);
+router.put('/updateBlog/:id', blogController.updateBlog);
 router.get('/getAllBlogs', blogController.getAllBlogs);
-router.get('/getBlog/:_id', blogController.getBlog);
-
+router.get('/getBlog/:id', blogController.getBlog);
+router.delete('/deleteBlog/:id', blogController.deleteBlog);
 
 module.exports = router;
